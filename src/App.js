bc433abb9f129374771f134f4b376dfca271e5db
Natnael Tekletsadik <isaacnatnael@gmail.com>
2022-03-22 21:00:31 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useEffect } from 'react';
+import './App.css';
+import scrollAnimation from './scrollAnimation';
+
+function App() {
+
+  useEffect(() => {
+    scrollAnimation();
+  }, [])
+
+  return (
+    <div className="App">
+
+      <div class="name scroll">
+        <h1 class="N ">N</h1>
+        <h1 class="A">A</h1>
+        <h1 class="T">T</h1>
+        <h1 class="I">I</h1>
+      </div>
+      <div class="name second">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, quibusdam!
+      </div>
+      <div class="name two">
+        <h1 class="N ">N</h1>
+        <h1 class="A">A</h1>
+        <h1 class="T">T</h1>
+        <h1 class="I">I</h1>
+      </div>
+      <div class="name three">
+        <h1 class="N ">N</h1>
+        <h1 class="A">A</h1>
+        <h1 class="T">T</h1>
+        <h1 class="I">I</h1>
+      </div>
+
+      {/* <div class="square-wrapper page show-in ">
+    <div class="square"></div>
+  </div>
+
+  <div class="square-wrapper2 page ">
+    <div class="square2">2nd square</div>
+  </div>  */}
+
+      {/* <div class="name third">
+        <h1 class="N ">N</h1>
+        <h1 class="A">A</h1>
+        <h1 class="T">T</h1>
+        <h1 class="I">I</h1>
+      </div> */}
+
+      {/* <div class="name last">
+        <h1 class="N ">N</h1>
+        <h1 class="A">A</h1>
+        <h1 class="T">T</h1>
+        <h1 class="I">I</h1>
+      </div> */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/scrollAnimation.js
@@ -0,0 +1,73 @@
+import $ from 'jquery';
+
+export default function scrollAnimation () {
+  
+  
+    console.log('vh', window.innerHeight);
+    const divs = $('.scroll');
+    const sec = $('.second');
+    const two = $('.two');
+    const three = $('.three');
+  
+    $(window).on('scroll', function () {
+  
+      const vh = window.innerHeight;
+      const vh2 = window.innerHeight / 2;
+      const st = $(this).scrollTop();
+      // console.log('scroll y and scrollTop()',window.scrollY, st)
+      const num = 100;
+      const top = 100;    // inital top value
+      //  console.log('top offset',$('.last').offset());
+  
+      // sec.map((index, element) => {
+      //   console.log(element)
+      // if(window.scrollY + (vh / 2) >= sec.offset().top ) {
+      //   console.log(`element  at half`);
+      //   sec.css({ 'opacity': 1  })
+      // }
+  
+      // })
+      divs.css({
+        'opacity': (1 - st / num),
+        'top': `${100 + window.scrollY}px `
+      });
+  
+      // fade in
+      if (window.scrollY + (vh / 2) >= 600) {  // div at half viewport
+        sec.css({
+          'opacity': ((st + vh2 - 600) / (vh2 - 100))
+        });
+      }
+      else {
+        sec.css({
+          'opacity': 0
+        });
+      }
+  
+      // fade out
+      if (window.scrollY + 100 >= 600) {  // div at 100 from top
+        sec.css({
+          'opacity': (1 - (st - 600) / num),
+          'top': `${100 + window.scrollY}px `
+        });
+      }
+  
+  
+      if (window.scrollY + 100 >= 1100) {
+        two.css({
+          'opacity': (1 - (st - 1100) / num),
+          'top': `${100 + window.scrollY}px `
+        });
+      }
+      if (window.scrollY + 100 >= 1600) {
+        three.css({
+          'opacity': (1 - (st - 1600) / num),
+          'top': `${100 + window.scrollY}px `
+        });
+      }
+  
+  
+    });
+  
+  
+};
