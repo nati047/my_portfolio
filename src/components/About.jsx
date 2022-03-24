992e09831fd6cdc4036b0b354abe92d092bc3761
Natnael Tekletsadik <isaacnatnael@gmail.com>
2022-03-24 18:00:16 -0400

    about page layout done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function About () {
   return (
-    <div id="about" className="about-container"></div>
+    <div id="about" className="about-container">
+      <div className="about-me">ABOUT&nbsp;ME
+        <div className="underline"></div>
+      </div>
+      <p className="about-paragraph">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores repellat qui aliquid provident error blanditiis unde velit! Eos eum quisquam dicta placeat illum? Laborum, a, totam fugit inventore velit sapiente explicabo deleniti itaque delectus consequatur fuga architecto animi eligendi? Odio.
+      </p>
+    </div>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,10 +5,10 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
-function Home () {
+function Home() {
   return (
-    <div id="home" className="grid-home">
-      <div className="nav">
+    <div className="home">
+      <div className="nav">  {/* @todo make navbar component*/}
         <div className="nav-menu">
           <a href="#home" className="nav-item">Home</a>
           <a href="#about" className="nav-item">About</a>
@@ -16,23 +16,26 @@ function Home () {
           <a className="nav-item">Projects</a>
         </div>
         <div className="socials">
-          <a href="https://github.com/nati047"><FontAwesomeIcon icon={ faGithub } /></a>
-          <a href="https://www.linkedin.com/in/webdev-nat/"><FontAwesomeIcon icon={ faLinkedin } /></a>
-          <a><FontAwesomeIcon icon={ faInstagram } /></a>
+          <a href="https://github.com/nati047"><FontAwesomeIcon icon={faGithub} /></a>
+          <a href="https://www.linkedin.com/in/webdev-nat/"><FontAwesomeIcon icon={faLinkedin} /></a>
+          <a><FontAwesomeIcon icon={faInstagram} /></a>
         </div>
       </div>
 
 
-      <div className="home-main">
-        <div className="hi">Hi,</div>
-        <div className="name-container">
-          <div className="im-name">I'm&nbsp;Natnael&nbsp;Tekletsadik</div>
+      <div id="home" className="grid-home">
+        <div className="home-main">
+          <div className="hi">Hi,</div>
+          <div className="name-container">
+            <div className="im-name">I'm&nbsp;Natnael&nbsp;Tekletsadik</div>
+          </div>
+          <div className="developer">Full-stack Web Developer</div>
         </div>
-        <div className="developer">Full-stack Web Developer</div>
-      </div>
 
-      <div className="home-qoute">
-        <div className="qoute">eat&nbsp;&nbsp;sleep&nbsp;&nbsp;code!</div>
+        <div className="home-qoute">
+          <div className="qoute">eat&nbsp;&nbsp;sleep&nbsp;&nbsp;code!</div>
+        </div>
+
       </div>
 
     </div>
