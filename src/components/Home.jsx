b305ed731734042a57592e08432b2d31e78ded86
Natnael Tekletsadik <isaacnatnael@gmail.com>
2022-03-23 21:58:11 -0400

    home page content and basic styling done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,38 @@
 import '../styles/Home.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function Home () {
   return (
-    <div className="grid-home">
+    <div id="home" className="grid-home">
       <div className="nav">
-        <a className="nav-item">home</a>
-        <a className="nav-item">about</a>
-        <a className="nav-item">skills</a>
-        <a className="nav-item">projects</a>
+        <a href="#home" className="nav-item">Home</a>
+        <a className="nav-item">About</a>
+        <a className="nav-item">Skills</a>
+        <a className="nav-item">Projects</a>
       </div>
-      <div className="socials"></div>
+
+      <div className="socials">
+        <a href="https://github.com/nati047"><FontAwesomeIcon icon={ faGithub } /></a>
+        <a href="https://www.linkedin.com/in/webdev-nat/"><FontAwesomeIcon icon={ faLinkedin } /></a>
+        <FontAwesomeIcon icon={ faInstagram } />
+      </div>
+
       <div className="home-main">
         <div className="hi">Hi,</div>
-        <div className="im-name">I'm Natnael Tekletsadik</div>
+        <div className="name-container">
+          <div className="im-name">I'm&nbsp;Natnael&nbsp;Tekletsadik</div>
+        </div>
         <div className="developer">Full-stack Web Developer</div>
       </div>
+
       <div className="home-qoute">
-        <div className="qoute">eat, sleep, code</div>
+        <div className="qoute">eat&nbsp;&nbsp;sleep&nbsp;&nbsp;code!</div>
       </div>
+
     </div>
   );
 }
