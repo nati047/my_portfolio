f12e8dc1e2de44cef10f0bb4990ca330e321a7e2
Natnael Tekletsadik <isaacnatnael@gmail.com>
2022-03-31 22:11:03 -0400

    upload project images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,10 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import image1 from '../images/My-urls.png';
 import dog from '../images/dog.jpg';
+import backsteen from '../images/backsteen.png';
+import tinyApp from '../images/tinyapp.png';
+import scheduler from '../images/scheduler.png';
+import tweeter from '../images/tweeter.png';
 function Projects() {
   return (
     <div id="projects" className="projects-container container">
@@ -13,52 +17,52 @@ function Projects() {
       <div className="projects-flex">
         <div className="project-card">
           <div className="image-wrapper">
-            <img className="project-image" src={dog} />
+            <img className="project-image" src={backsteen} />
+          </div>
+          <div className="project-description">
+            <div className="project-name">BACKSTEEN</div>
+            <div className="about-project">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil error laborum distinctio repudiandae labore maiores autem velit optio fuga veritatis!</div>
+            <div className="project-links">
+              <a href="https://github.com/nati047/backsteen" ><FontAwesomeIcon icon={faGithub} /></a>
+              <a href="http://backsteen.herokuapp.com/"><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
+            </div>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="image-wrapper">
+            <img className="project-image" src={tinyApp} />
           </div>
           <div className="project-description">
             <div className="project-name">TINY APP</div>
             <div className="about-project">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil error laborum distinctio repudiandae labore maiores autem velit optio fuga veritatis!</div>
             <div className="project-links">
-              <a><FontAwesomeIcon icon={faGithub} /></a>
+              <a href="https://github.com/nati047/tinyapp" ><FontAwesomeIcon icon={faGithub} /></a>
+              <a href=""><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
+            </div>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="image-wrapper">
+            <img className="project-image" src={scheduler} />
+          </div>
+          <div className="project-description">
+            <div className="project-name">SCHEDULER</div>
+            <div className="about-project">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil error laborum distinctio repudiandae labore maiores autem velit optio fuga veritatis!</div>
+            <div className="project-links">
+              <a href="https://github.com/nati047/scheduler"><FontAwesomeIcon icon={faGithub} /></a>
               <a><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
             </div>
           </div>
         </div>
         <div className="project-card">
           <div className="image-wrapper">
-            <img className="project-image" src={dog} />
+            <img className="project-image" src={tweeter} />
           </div>
           <div className="project-description">
-            <div className="project-name">TINY APP</div>
+            <div className="project-name">TWEETER</div>
             <div className="about-project">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil error laborum distinctio repudiandae labore maiores autem velit optio fuga veritatis!</div>
             <div className="project-links">
-              <a><FontAwesomeIcon icon={faGithub} /></a>
-              <a><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
-            </div>
-          </div>
-        </div>
-        <div className="project-card">
-          <div className="image-wrapper">
-            <img className="project-image" src={dog} />
-          </div>
-          <div className="project-description">
-            <div className="project-name">TINY APP</div>
-            <div className="about-project">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil error laborum distinctio repudiandae labore maiores autem velit optio fuga veritatis!</div>
-            <div className="project-links">
-              <a><FontAwesomeIcon icon={faGithub} /></a>
-              <a><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
-            </div>
-          </div>
-        </div>
-        <div className="project-card">
-          <div className="image-wrapper">
-            <img className="project-image" src={image1} />
-          </div>
-          <div className="project-description">
-            <div className="project-name">TINY APP</div>
-            <div className="about-project">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil error laborum distinctio repudiandae labore maiores autem velit optio fuga veritatis!</div>
-            <div className="project-links">
-              <a><FontAwesomeIcon icon={faGithub} /></a>
+              <a href="https://github.com/nati047/tweeter" ><FontAwesomeIcon icon={faGithub} /></a>
               <a><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
             </div>
           </div>
