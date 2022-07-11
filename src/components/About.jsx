39aa395a3d26f2e795771890e20febcfa25eeca6
Natnael Tekletsadik <isaacnatnael@gmail.com>
2022-07-10 23:25:05 -0400

    update email and cleaup

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,4 @@
 import '../styles/About.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import mypic from '../images/mypic.jpg';
 
 function About () {
   return (
@@ -8,14 +6,10 @@ function About () {
       <div className="about-me">ABOUT&nbsp;ME
         <div className="underline"></div>
       </div>
-      {/* <div className="textNphoto">
-        <div className="photo-container">
-          <img className="mypic" src={mypic} alt="" />
-        </div> */}
         <p className="about-paragraph">
-          Hello, I'm Natnael Tekletsadik (call me Nat). I am a junior Full-stack web developer. I love turning well crafted code into beautiful and useful apps.
-          when I wrote my first calculator app, I felt like I can do anything. I can not only admire technology but also become a creator. For this reason I decided to do what I enjoy everyday and I studied full-stack web development. But, my journey just began.When i'm not infront of my screen I enjoy watching and playing soccer. 
-          Right now I'm looking for fulltime web developer positions. If you want to have a conversation I'm a text away. Enjoy checking out my projects! 
+          Hello, I'm Natnael Tekletsadik (friends call me Nat). I am a junior Full-stack web developer. I love turning well crafted code into beautiful and useful apps. Codimg has tranformed me from just admiring technology to also become a creator. For this reason I decided to do what I enjoy everyday and I studied full-stack web development. Right now I'm looking for fulltime web developer positions. 
+          When i'm not infront of my screen, I enjoy watching and playing soccer. 
+           If you want to have a conversation I'm a text away. Enjoy checking out my projects! 
         </p>
       {/* </div> */}
     </div>
