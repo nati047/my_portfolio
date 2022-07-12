c4620f46ea218fbf08eb38a97031b6cf6a0ee898
Natnael Tekletsadik <isaacnatnael@gmail.com>
2022-07-12 05:23:34 -0400

    about error fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About () {
         <div className="underline"></div>
       </div>
         <p className="about-paragraph">
-          Hello, I'm Natnael Tekletsadik (friends call me Nat). I am a junior Full-stack web developer. I love turning well crafted code into beautiful and useful apps. Coding has tranformed me from just admiring technology to also become a creator. For this reason I decided to do what I enjoy everyday and I studied full-stack web development. Right now I'm looking for fulltime web developer positions. 
+          Hello, I'm Natnael Tekletsadik (friends call me Nat). I am a junior Full-stack web developer. I love turning well crafted code into beautiful and useful apps. Coding  tranformes from just admiring technology to becoming a creator. For this reason I decided to do what I love everyday and I studied full-stack web development. Right now I'm looking for fulltime web developer positions. 
           When i'm not infront of my screen, I enjoy watching and playing soccer. 
            If you want to have a conversation I'm a text away. Enjoy checking out my projects! 
         </p>
